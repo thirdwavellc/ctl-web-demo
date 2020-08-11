7156b361163c0cb9e3bce60e0086bff125909162
Terry Fadz <terry@glantz.net>
2020-08-11 11:16:34 -0500

    added version 2 of rolodex

---
+++ rolodex-v2/js/demo.js
@@ -0,0 +1,46 @@
+var CoolScroll = (function( $ ) {
+  var init = function() {
+    pin_sections();
+    pin_images();
+  },
+
+  pin_sections = function() {
+
+	TweenLite.defaultEase = Linear.easeNone;
+	var controller = new ScrollMagic.Controller();
+
+	$(".section").each(function(i) {
+		 // $(this).prev().find('.box').scrollTop(0);
+	  var target = $(this).find(".box");
+	  var tl = new TimelineMax();
+	 	
+	 	new ScrollMagic.Scene({
+	    triggerElement: this,
+	    triggerHook: 0.2,
+	    duration: '100%'
+	  })
+	    .setPin(this)
+	    .setTween(tl)
+	    .addIndicators()
+	    .addTo(controller);
+	});
+
+},
+
+pin_images = function() {
+	// var controller = new ScrollMagic.Controller();
+	// 	tween = TweenMax.to(".img", .5, {position: "fixed"});
+	// 	new ScrollMagic.Scene({
+	// 		triggerElement: this,
+	// 		duration: '100%'
+	// 	})
+	// 	.setTween(tween)
+	// 	.addTo(controller);
+}
+
+return {
+	init: init
+};
+})( jQuery );
+
+jQuery( window ).on( 'load', CoolScroll.init );
